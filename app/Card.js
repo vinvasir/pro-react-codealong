6919a3aba7e54155960da827350d4edfdbc0b7de
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-11-14 11:37:01 -0500

    added a showDetails property to the card state and created an event handler to toggle card state

--- app/Card.js
+++ app/Card.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
 import CheckList from './CheckList';
 
 class Card extends Component {
+	constructor() {
+		super();
+		this.state = {
+			showDetails: false
+		};
+	}
+
 	render() {
-		return (
-			<div className="card">
-				<div className="card__title">
-					{this.props.title}
-				</div>
+		let cardDetails;
+		if (this.state.showDetails) {
+			cardDetails = (
 				<div className="card__details">
 					{this.props.description}
-					<CheckList cardId={this.props.id} 
-						tasks={this.props.tasks} />
+					<CheckList cardId={this.props.id} tasks={this.props.tasks} />
 				</div>
+			);
+		}
+
+		return (
+			<div className="card">
+				<div className="card__title" onClick={
+					() => this.setState({showDetails: !this.state.showDetails})
+				}>
+					{this.props.title}
+				</div>
+				{cardDetails}
 			</div>
 		);
 	}
