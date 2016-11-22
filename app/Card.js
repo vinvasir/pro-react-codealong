3d4f9ad25f15e4008192e01cfde25810e10abe5d
Prateek Vasireddy <prat.vasireddy@gmail.com>
2016-11-22 14:00:44 -0500

    fixed typo in 'typeof' keyword

--- app/Card.js
+++ app/Card.js
@@ -5,7 +5,7 @@ let titlePropType = (props, propName, componentName) => {
 	if (props[propName]) {
 		let value = props[propName];
 
-		if(typeOf value !== 'string' || value.length > 80) {
+		if(typeof value !== 'string' || value.length > 80) {
 			return new Error(
 				'${propName} in ${componentName} is longer than 80 characters'
 			);
